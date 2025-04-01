ded4f783998b83e7c770bb88f6aae2cfe9669a3f
Ryan <ryanloi273@gmail.com>
2025-04-01 00:22:38 -0600

    fixed tailwind config file to export modules, added some canvas components, added index.js files to help facilitate exporting of components.
    Started to modify app.jsx to include browserr router and import components that we may begin to use

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,16 @@
+import { BrowserRouter } from "react-router-dom";
+import {About, Contact, Experience, Feedback, Hero, Navbar, Tech, Work, StarsCanvas} from "./components";
+
 const App = () => {
   return (
-    <div>Ryan Loi's 3D Developer Portfolio</div>
+    <BrowserRouter>
+      <div className = "relative z-0 bg-primary"> 
+        <div className = "bg-hero-pattern bg-cover bg-no-repeat bg-center">
+          <Navbar />
+          <Hero />
+        </div>
+      </div>
+    </BrowserRouter>
   )
 }
 
---
+++ src/components/index.js
@@ -0,0 +1,29 @@
+// Index file for exporting components
+
+import { EarthCanvas, BallCanvas, ComputersCanvas, StarsCanvas } from './canvas';
+import Hero from "./Hero";
+import Navbar from "./Navbar";
+import About from "./About";
+import Tech from "./Tech";
+import Experience from "./Experience";
+import Work from "./Work";
+import Feedback from "./Feedback";
+import Contact from "./Contact";
+import CanvasLoader from "./Loader";
+
+
+export {
+  Hero,
+  Navbar,
+  About,
+  Tech,
+  Experience,
+  Work,
+  Feedback,
+  Contact,
+  CanvasLoader,
+  EarthCanvas, 
+  BallCanvas, 
+  ComputersCanvas, 
+  StarsCanvas
+};
