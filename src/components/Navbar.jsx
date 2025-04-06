61f46ad82cf70ed020c64541675671aa05e1db73
Ryan <ryanloi273@gmail.com>
2025-04-06 04:06:54 -0600

    adjusted nav bar padding and logo size

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,13 +10,13 @@ const Navbar = () => {
     const [isMobile, setIsMobile] = useState(false);
 
     return (
-        <nav className={`${styles.paddingX} w-full flex items-center py-5 top-10 z-50 bg-primary`}>            
+        <nav className={`${styles.paddingX} w-full flex items-center py-2 top-10 z-50 bg-primary`}>            
 
-            <div className = "w-full flex justify-between items-center max-w-7xl mx-auto">
+            <div className = "w-full flex justify-between items-center max-w-full mx-auto">
                 <Link to="/" className = "flex items-center gap-2" 
                     onClick = {() => {setActive(""); window.scrollTo(0, 0);}} 
                 >
-                    <img src={logo} alt="profile logo" className = "w-9 h-9 object-contain"></img>
+                    <img src={logo} alt="profile logo" className = "w-12 h-12 object-contain"></img>
                     <p className="text-white text-[18px] font-bolt cursor-pointer flex">Ryan Loi&nbsp;<span className = "sm:block hidden">| Software Developer</span></p>
                 </Link>
                 <ul className='list-none hidden sm:flex flex-row gap-10'>
@@ -44,7 +44,7 @@ const Navbar = () => {
                     onClick={() => setIsMobile(!isMobile)}
                     />
 
-                    <div className = {`${!isMobile ? "hidden" : "flex"} p-6 black-gradient absolute top-12 right-0 mx-4 my-2 min-w-[115px] z-10 rounded-xl`}>
+                    <div className = {`${!isMobile ? "hidden" : "flex"} p-6 black-gradient absolute top-12 right-0 mx-4 my-4 min-w-[125px] z-10 rounded-xl`}>
                         <ul className='list-none flex justify-end items-start flex-col gap-4'>
                             {navLinks.map((link) => (
                                 <li 
