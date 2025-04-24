f516db401376fddf89a82f93d07a71145b1baee7
Ryan <ryanloi273@gmail.com>
2025-04-24 16:33:06 -0600

    created initial experience card

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -11,9 +11,27 @@ import { experiences } from "../constants";
 import { SectionWrapper } from "../hoc";
 import { textVariant } from "../utils/motion";
 
-
-
-
+const ExperienceCard = ({ experience }) => (
+  <VerticalTimelineElement
+    contentStyle={{ background: "#1d1836", color: "#fff" }}
+    contentArrowStyle={{ borderRight: "7px solid #232631" }}
+    date={experience.date}
+    iconStyle={{ background: experience.iconBg }}
+    icon={
+      <div>
+        <img
+          src={experience.icon}
+          alt={experience.company_name}
+          className="w-[60%] h-[60%] object-contain"
+        />
+      </div>
+    }
+  >
+    <div>
+      <h3 className="text-white text-[24px] font-bold">{experience.title}</h3>
+    </div>
+  </VerticalTimelineElement>
+);
 
 const Experience = () => {
   return (
@@ -30,12 +48,11 @@ const Experience = () => {
 
       <div className="mt-20 flex flex-col">
         <VerticalTimeline>
-            {experiences.map((experience, index) => (
-                <ExperienceCard key={index} experience={experience}/>
-            ))}
+          {experiences.map((experience, index) => (
+            <ExperienceCard key={index} experience={experience} />
+          ))}
         </VerticalTimeline>
       </div>
-
     </>
   );
 };
