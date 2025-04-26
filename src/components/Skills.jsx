0f103a33ccfe0a7e638266322498ed7496fd9438
Ryan <ryanloi273@gmail.com>
2025-04-25 23:15:49 -0600

    created initial skills framework

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,9 +1,28 @@
-import React from 'react';
+import { BallCanvas } from "./canvas";
+import { SectionWrapper } from "../hoc";
+import { technologies } from "../constants";
+import { styles } from "../styles";
+import { motion } from "framer-motion";
+import { textVariant, fadeIn } from "../utils/motion";
 
 const Skills = () => {
-    return (
-        <div>Tech</div>
-    )
-}
+  return (
+    <>
+      <motion.div
+        variants={textVariant()}
+        initial="hidden"
+        whileInView="show"
+        viewport={{ once: true, amount: 0.2 }}
+      >
+        <p className={styles.sectionSubText}>What I Bring to the Table</p>
+        <h2 className={styles.sectionHeadText}>Software Development Skills.</h2>
+      </motion.div>
 
-export default Skills;
+
+      
+    </>
+  );
+};
+
+const WrappedSkills = SectionWrapper(Skills, "skills");
+export default WrappedSkills;
