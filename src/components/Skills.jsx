974fd499ba3ffd58238f13f7d4e8d9c8af320fb3
Ryan <ryanloi273@gmail.com>
2025-04-27 00:29:34 -0600

    updated skills section to map all the individual categories of technologies

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -3,7 +3,7 @@ import { SectionWrapper } from "../hoc";
 import { technologies } from "../constants";
 import { styles } from "../styles";
 import { motion } from "framer-motion";
-import { textVariant, fadeIn } from "../utils/motion";
+import { textVariant } from "../utils/motion";
 
 const Skills = () => {
   return (
@@ -18,20 +18,28 @@ const Skills = () => {
         <h2 className={styles.sectionHeadText}>Technical Skills.</h2>
       </motion.div>
 
-      <motion.div
-        variants={textVariant()}
-        initial="hidden"
-        whileInView="show"
-        viewport={{ once: true, amount: 0.2 }}
-      >
-        <div className="flex flex-row flex-wrap justify-center gap-10">
-          {technologies.map((technology) => (
-            <div className="w-28 h-28" key={technology.name}>
-              <BallCanvas icon={technology.icon} link={technology.link} />
-            </div>
-          ))}
-        </div>
-      </motion.div>
+      {Object.entries(technologies).map(([category, items]) => (
+        <motion.div
+          key={category}
+          variants={textVariant()}
+          initial="hidden"
+          whileInView="show"
+          viewport={{ once: true, amount: 0.2 }}
+          className="my-10"
+        >
+          <h3 className="text-center text-2xl font-bold capitalize mb-6">
+            {category.replace(/([A-Z])/g, " $1")}
+          </h3>
+
+          <div className="flex flex-row flex-wrap justify-center gap-10">
+            {items.map((technology) => (
+              <div className="w-28 h-28" key={technology.name}>
+                <BallCanvas icon={technology.icon} link={technology.link} />
+              </div>
+            ))}
+          </div>
+        </motion.div>
+      ))}
     </>
   );
 };
