a4e99f4a918123191e25850c214f2928d2ff7482
Ryan <ryanloi273@gmail.com>
2025-04-28 23:33:47 -0600

    finished all of the Work section

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -20,8 +20,26 @@ const Work = () => {
         <h2 className={styles.sectionHeadText}>Projects.</h2>
       </motion.div>
 
+      <div className="w-full flex">
+        <motion.p
+          variants={fadeIn("", "", 0.1, 1)}
+          className="mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]"
+        >
+          The following projects showcase my technical skills and hands-on
+          experience through real-world applications. Each includes a brief
+          description, along with links to the GitHub repository and live demo
+          where available. Together, they demonstrate my ability to solve
+          complex problems, work across a range of technologies, and manage
+          projects from concept to completion.
+        </motion.p>
+      </div>
 
+      <div className = "mt-20 flex flex-wrap gap-7">
+        {projects.map((project, index) =>(
+          <ProjectCard key = {`project-${index}`} {...project} index={index}/>
+        ))}
 
+      </div>
     </>
   );
 };
