a909361aba44d77e2e2518ce56777eeac547cd2b
Ryan <ryanloi273@gmail.com>
2025-04-28 20:57:06 -0600

    creating the initial work section to display my projects

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,10 +1,30 @@
-import React from 'react';
+import Tilt from "react-parallax-tilt";
+import { motion } from "framer-motion";
+
+import { styles } from "../styles";
+import { github } from "../assets";
+import { SectionWrapper } from "../hoc";
+import { projects } from "../constants";
+import { fadeIn, textVariant } from "../utils/motion";
 
 const Work = () => {
-    return (
-        <div>Work</div>
-    )
-}
+  return (
+    <>
+      <motion.div
+        variants={textVariant()}
+        initial="hidden"
+        whileInView="show"
+        viewport={{ once: true, amount: 0.2 }}
+      >
+        <p className={styles.sectionSubText}>My Work</p>
+        <h2 className={styles.sectionHeadText}>Projects.</h2>
+      </motion.div>
 
-export default Work;
 
+
+    </>
+  );
+};
+
+const WrappedWork = SectionWrapper(Work, "projects");
+export default WrappedWork;
